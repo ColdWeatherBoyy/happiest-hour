39f032b44876c569be28f27cd4aa10e5c717e603
Liza Parr <lizajparr@gmail.com>
2023-06-21 12:35:24 -0400

    submit form and button centered and shortened length

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import SubmitForm from "../SubmitForm/SubmitForm";
 import YelpList from "../YelpList/YelpList";
 import Clock from "../Clock/Clock";
-import Title from "./Title";
+import Title from "../Title/Title";
 
 const Home = () => {
 	// const [isClicked, setIsClicked] = useState(false);
