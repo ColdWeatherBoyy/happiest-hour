a91bc18c6490b3bfdd70b52fac2d8c912697931c
Liza Parr <lizajparr@gmail.com>
2023-06-22 15:42:40 -0400

    starts translation calcs for numbers

--- client/src/components/Clock/Numbers/NumberItem.jsx
+++ client/src/components/Clock/Numbers/NumberItem.jsx
@@ -1,5 +1,27 @@
-const NumberItem = () => {
+const NumberItem = ({ numbersArr }) => {
     const calcTranslation = () => {
-        // insert calcs here
+
+        if(numbersArr[i] <= 6) {
+            const x = () => {
+                x = x+15
+            }
+        } else if(6 < numbersArr[i] <= 11) {
+            let x = -1*x
+        }
     }
+
+    return(
+        {
+            x,
+            y
+        }
+    )
 }
+
+// I need the <p> with the numbers showing up from the numbersArr to each 
+// have a {{style: transform: translate(x,y)}}
+
+// so this component (NumberItem) should return a <p style={{transform: "{x}%","{y}%"}}>{num}</p>
+
+// maybe try setting all the numbers in the middle, then they only need to travel 50% of 
+// the container (the circle) and x or y units up, down, left, or right.
--- client/src/components/Clock/Numbers/Numbers.jsx
+++ client/src/components/Clock/Numbers/Numbers.jsx
@@ -1,7 +1,7 @@
 import './Numbers.css'
 
 const Numbers = () => {
-    const numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    const numbersArr = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     return(
         <div>
             {numbersArr.map((num, id) => {
