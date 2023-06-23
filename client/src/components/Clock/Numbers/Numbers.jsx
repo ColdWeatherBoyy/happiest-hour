d3d9198d3da7baee4d5d3487716de23275ce1c5f
Liza Parr <lizajparr@gmail.com>
2023-06-23 15:23:31 -0400

    troubleshooting calculations

--- client/src/components/Clock/Numbers/Numbers.jsx
+++ client/src/components/Clock/Numbers/Numbers.jsx
@@ -4,12 +4,19 @@ import NumberItem from './NumberItem'
 const Numbers = () => {
     const numbersArr = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-    let xvalue = 50;
-    let yvalue = 50
+    let xvalue;
+    let yvalue;
 
     return(
         <div>
             {numbersArr.map((arrayNum, id) => {
+
+                if((3 < arrayNum) && (arrayNum < 7)) {
+                    console.log(arrayNum);
+                    xvalue = 70;
+                    yvalue = 50;
+                }
+
                 return(
                     <NumberItem x={xvalue} y={yvalue} num={arrayNum} key={id}/>
                 )
