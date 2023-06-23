70a93f5ee6ac7783ee779dc38bc3f3391e1837cd
Liza Parr <lizajparr@gmail.com>
2023-06-23 14:26:52 -0400

    consolidates functions, moves translation calcs to Numbers component

--- client/src/components/Clock/Numbers/NumberItem.jsx
+++ client/src/components/Clock/Numbers/NumberItem.jsx
@@ -1,18 +1,4 @@
-const NumberItem = ({ num }) => {
-
-    const calcTranslation = () => {
-        let x = 0;
-        let y = 0;
-        
-        return(
-            {
-                x,
-                y
-            }
-        )    
-    }
-
-    let { x, y } = calcTranslation();
+const NumberItem = ({ num, x, y }) => {
 
     return(
         <p style={{position: `absolute`, left: `${x}%`, top: `${y}%`, zIndex: `1`, transform: `translate(-50%, -50%)` }}>{num}</p>
@@ -22,13 +8,3 @@ const NumberItem = ({ num }) => {
 }
 
 export default NumberItem;
-
-
-
-// calcTranslations calculates x and y translations
-    // either convert them to strings or use '' in style={...}
-    // return x & y-values
-// NumberItem returns <p style={{transform: `translate(${x}%, ${y}%)` }}>{num}</p> with 'num' as a prop
-
-// render <NumberItem key={id} /> in Numbers
-// render the <Numbers className='numbers' /> in Clock
--- client/src/components/Clock/Numbers/Numbers.jsx
+++ client/src/components/Clock/Numbers/Numbers.jsx
@@ -4,11 +4,14 @@ import NumberItem from './NumberItem'
 const Numbers = () => {
     const numbersArr = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
+    let xvalue = 50;
+    let yvalue = 50
+
     return(
         <div>
             {numbersArr.map((arrayNum, id) => {
                 return(
-                    <NumberItem num={arrayNum} key={id}/>
+                    <NumberItem x={xvalue} y={yvalue} num={arrayNum} key={id}/>
                 )
             })}
         </div>
