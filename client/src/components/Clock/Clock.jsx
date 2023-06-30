239affb80788ab83b0b05df7492f7a4cfd709ebd
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-06-30 12:56:22 -0400

    pair programming to adjust location of inner circle dots and center of second, minute, hour hand rotation

--- client/src/components/Clock/Clock.jsx
+++ client/src/components/Clock/Clock.jsx
@@ -1,18 +1,18 @@
 import "./Clock.css";
-import Numbers from './Numbers/Numbers';
+import Numbers from "./Numbers/Numbers";
 import ClockHands from "./ClockHands/ClockHands";
 import SubmitForm from "../SubmitForm/SubmitForm";
 
 const Clock = ({ setSubmittedZip }) => {
-
 	return (
 		<div className="clock-frame">
-            <Numbers />
-            <ClockHands />
+			{/* <div className="vertical-line"></div>
+			<div className="horizontal-line"></div> */}
+			{/* <Numbers /> */}
+			<ClockHands />
 			<SubmitForm setSubmittedZip={(zipCode) => setSubmittedZip(zipCode)} />
-        </div>
-    )
-	
+		</div>
+	);
 };
 
 export default Clock;
