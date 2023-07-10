6a25c012bb75f6eab69cb98de7dd4c0812bce3a9
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-07-10 14:30:47 -0400

    updates to using useref for card placement. rough positioning – need to figure out what the values are

--- client/src/components/Clock/BarCard/Card.jsx
+++ client/src/components/Clock/BarCard/Card.jsx
@@ -1,15 +1,16 @@
 function Card({ name, rating, xval, yval }) {
+	const translateX = `calc(${xval}px - 63%)`;
+	const translateY = `calc(${yval}px - 50%)`;
 	return (
 		<div
 			style={{
 				position: `absolute`,
 				zIndex: `2`,
-				bottom: `50%`,
-				left: `50%`,
 				border: `1px solid green`,
 				width: `20%`,
 				textAlign: `center`,
-				transform: `translateX(${xval}px) translateY(${yval}px)`,
+				// right: `100%`,
+				transform: `translateX(${translateX}) translateY(${translateY})`,
 			}}
 		>
 			<p>{name}</p>
