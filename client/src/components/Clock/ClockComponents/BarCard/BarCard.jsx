d3525805eb2c40ab64f5ce0268a7b9698d122960
Liza Parr <lizajparr@gmail.com>
2023-07-22 17:01:49 -0400

    styles bar card colors and shape

--- client/src/components/Clock/ClockComponents/BarCard/BarCard.jsx
+++ client/src/components/Clock/ClockComponents/BarCard/BarCard.jsx
@@ -5,15 +5,31 @@ function BarCard({ name, rating, xval, yval }) {
         position: `absolute`,
         zIndex: `2`,
         // border: `1px solid green`,
-        background: 'black',
-        borderRadius: '5%',
-        color: 'whitesmoke',
-        width: `20%`,
-        height: `20%`,
+        background: "rgb(255, 186, 108)",
+        borderRadius: "50%",
+        color: "rgb(22, 96, 12)",
+        // width: `30%`,
+        // height: `30%`,
+        width: `25%`,
+        height: `25%`,
         textAlign: `center`,
         transform: `translateX(${xval}px) translateY(${yval}px)`,
       }}
     >
+      <div
+        style={{
+          width: "80%",
+          height: "80%",
+          backgroundColor: "rgb(255, 162, 55)",
+          position: "absolute",
+          borderRadius: "50%",
+          zIndex: "-1",
+          transformOrigin: 'center',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}
+      ></div>
       <p
         style={{
           marginBottom: "0",
@@ -25,7 +41,9 @@ function BarCard({ name, rating, xval, yval }) {
         style={{
           marginBottom: "0",
         }}
-      >{rating}</p>
+      >
+        {rating}
+      </p>
     </div>
   );
 }
