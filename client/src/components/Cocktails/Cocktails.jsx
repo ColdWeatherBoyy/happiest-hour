fbf078b03fa484fb0faf0732301a176cbe7d29f1
Liza Parr <lizajparr@gmail.com>
2023-07-24 12:08:18 -0400

    makes cocktail images better suited for screen width

--- client/src/components/Cocktails/Cocktails.jsx
+++ client/src/components/Cocktails/Cocktails.jsx
@@ -1,18 +1,22 @@
 import './Cocktails.css';
+import getWindowDimensions from '../../utils/hooks/useWindowDimensions';
 import cocktail1 from '../../assets/cocktail1.png';
 import cocktail2 from '../../assets/cocktail2.png';
 import cocktail3 from '../../assets/cocktail3.png';
 import cocktail4 from '../../assets/cocktail4.png';
 import cocktail5 from '../../assets/cocktail5.png';
 
-function Cocktails () {return(
-    <div className="cocktails">
-        <img src={cocktail1} alt="cocktail" />
-        <img src={cocktail4} alt="cocktail" />
-        <img src={cocktail3} alt="cocktail" />
-        <img src={cocktail2} alt="cocktail" id='orange' />
-        <img src={cocktail5} alt="cocktail" />
-    </div>
+function Cocktails () {
+    const { width, height } = getWindowDimensions();
+    
+    return(
+        <div className="cocktails">
+            <img src={cocktail1} alt="cocktail" />
+            <img src={cocktail4} alt="cocktail" />
+            <img src={cocktail3} alt="cocktail" />
+            <img src={cocktail2} alt="cocktail" id='orange' />
+            <img src={cocktail5} alt="cocktail" />
+        </div>
 )}
 
 export default Cocktails;
