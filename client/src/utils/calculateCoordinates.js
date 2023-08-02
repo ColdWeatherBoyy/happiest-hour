7793fa67faa921006624b0109b42168b49872c56
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-08-02 11:13:14 -0400

    adjust media queries for bar text

--- client/src/utils/calculateCoordinates.js
+++ client/src/utils/calculateCoordinates.js
@@ -1,10 +1,11 @@
 // function that calculates the coordinates for the numbers/cards on the clock
 const calculateCoordinates = (containerWidth) => {
+	console.log(containerWidth);
 	const radius = containerWidth / 2;
 
 	// Get the dimention of half of the container width, used to subtract from the xval & yval
 	// const halfCardWidth = 3 * (containerWidth / 20);
-	const halfCardWidth = (containerWidth / 8);
+	const halfCardWidth = containerWidth / 8;
 
 	// Define thetas as an array of 12 angles in radians
 	let thetas = Array.from({ length: 12 }).map((_, index) => {
